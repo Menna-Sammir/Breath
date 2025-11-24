f67623aa051d0c906b21bccded13785ecf1458b8
menna samir <36510244+Menna-Sammir@users.noreply.github.com>
2025-11-24 18:59:02 +0200

    feat(db): handel database

--- API/Program.cs
+++ API/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddControllers(opt =>
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     // opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
 builder.Services.AddSignalR();
